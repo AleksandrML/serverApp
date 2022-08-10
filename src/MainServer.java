84b40a6a4497f9b8a53cc8bcfc15478118071a10
aleksandr <ershets@gmail.com>
2022-08-10 22:30:01 +0300

    refactor

--- src/MainServer.java
+++ src/MainServer.java
@@ -8,19 +8,17 @@ import java.net.Socket;
 public class MainServer {
 
     public static void main(String[] args) throws IOException {
-        try (ServerSocket serverSocket = new ServerSocket(8080)) {
-            while (true) {
-                Socket clientSocket = serverSocket.accept(); // ждем подключения
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                System.out.println("New connection accepted");
-                final String name = in.readLine();
-                out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-                out.close();
-                in.close();
-            }
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(8080);
+                 Socket clientSocket = serverSocket.accept(); // ждем подключения
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                 ) {
+                    System.out.println("New connection accepted");
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                }
         }
-
     }
 
 }
